eabea13bfbe2f1713f908182c05a6d0a118d4c86
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-05-08 02:16:22 +0600

    server make done and mongodb add and mongodb find data user and clinet res sent user collection

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const app = express()
+const cors = require('cors')
+const port = process.env.PROT || 7001
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.USERNAMEDB}:${process.env.PASSWORDDB}@cluster0.tcghrel.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+const run = async () => {
+
+    try {
+
+        await client.connect();
+        await client.db('admin').command({ ping: 1 })
+        console.log('Pinged your deployment. You successfully connected to MongoDB!');
+
+        const db = client.db('express-simplecrud')
+        const userCollaction = db.collection('user')
+
+        app.get('/user', async (req, res) => {
+            const cursor = userCollaction.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+    }
+
+    finally {
+        // await client.close()
+    }
+
+
+
+}
+run().catch(console.dir)
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('simple crud server')
+})
+
+
+app.listen(port, () => {
+    console.log(`server is runing on port ${port}`);
+
+})
